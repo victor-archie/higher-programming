be7de89617f698a2a9a6ae24a691887d883fd16d
victor-archie <victorarchie50@gmial.com>
2023-08-10 19:49:47 -0700

    loops and conditionals

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 
 for i in range(97, 123):
     if i != 113 and i != 101:
-        print(f"{i:c}", end=" ")
+        print("{:c}".format(i), end=" ")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(0, 99):
-    print(f"{i:d} = 0x{i:x}")
+    print("{} = 0x{:x}".format(i, i)
